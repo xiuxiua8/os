4b453fa2036acd84ce9dfff4507cf17b88ac4310
Zilong <1831894878@qq.com>
2024-10-29 10:25:31 +0800

    lab1: add system and exec

--- lab1/pid.c
+++ lab1/pid.c
@@ -1,7 +1,8 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
-#include <sys/wait.h> 
 #include <unistd.h>
+#include <stdlib.h>
 
 int global = 10;
 
@@ -17,22 +18,25 @@ int main()
         return 1;
     }
     else if (pid == 0) { /* child process */
-        sleep(5);
-        printf("slept for 5 secends\n");
         global += 5;
         pid1 = getpid();
         printf("child: pid = %d\n", pid);    /* A */
         printf("child: pid1 = %d\n", pid1);  /* B */
         printf("the global variable in child process is %d, address is %p\n", global, (void*)&global);
+        printf("system: \n");
+        system("./call");
+        printf("exec: \n");
+        execl("./call", "call", NULL);
+
+        printf("This message is not supposed to be printed if exec succeeds.", global, (void*)&global);
     }
     else { /* parent process */
         global -= 5;
         pid1 = getpid();
         printf("parent: pid = %d\n", pid);   /* C */
         printf("parent: pid1 = %d\n", pid1); /* D */
-        wait(NULL);
         printf("the global variable in parent process is %d, address is %p\n", global, (void*)&global);
-        
+        wait(NULL);
     }
 
     return 0;
