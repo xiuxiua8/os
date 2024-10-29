1f56fadee5f7409a72c32653d92716dfbaff35f6
Zilong <1831894878@qq.com>
2024-10-29 22:12:08 +0800

    lab 1: pthread_cond_t

--- lab1/t.c
+++ lab1/t.c
@@ -4,22 +4,43 @@
 
 int shared_var = 0;
 pthread_mutex_t lock;
+pthread_cond_t cond;
+int turn = 0;  //0 increment 1 decrement 
 
 void *increment(void *param) {
-    for (int i = 0; i < 500000; i++) {
-        //pthread_mutex_lock(&lock);
+    for (int i = 0; i < 5000; i++) {
+        pthread_mutex_lock(&lock);
+
+        while (turn != 0) {  
+            pthread_cond_wait(&cond, &lock);
+        }
+
         shared_var++;
-        //pthread_mutex_unlock(&lock);
+        printf("Increment: shared_var = %d\n", shared_var);
+
+        turn = 1;  
+        pthread_cond_signal(&cond);  
+
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 
 void *decrement(void *param) {
-    printf("shared_var before decrement begins = %d\n", shared_var);
-    for (int i = 0; i < 500000; i++) {
-        //pthread_mutex_lock(&lock);
+    for (int i = 0; i < 5000; i++) {
+        pthread_mutex_lock(&lock);
+
+        while (turn != 1) { 
+            pthread_cond_wait(&cond, &lock);
+        }
+
         shared_var--;
-        //pthread_mutex_unlock(&lock);
+        printf("Decrement: shared_var = %d\n", shared_var);
+
+        turn = 0; 
+        pthread_cond_signal(&cond);  
+
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -28,6 +49,7 @@ int main() {
     pthread_t tid1, tid2;
 
     pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
 
     pthread_create(&tid1, NULL, increment, NULL);
     pthread_create(&tid2, NULL, decrement, NULL);
@@ -38,6 +60,7 @@ int main() {
     printf("Final value of shared_var = %d\n", shared_var);
 
     pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
 
     return 0;
 }
